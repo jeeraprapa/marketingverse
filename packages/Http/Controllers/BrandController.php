96f34da581e14d015966032b834b2198141151a3
jeeraprapa <jeeraprapa.h@gmail.com>
2023-11-10 22:23:28 +0700

    add visitor counter

--- packages/Http/Controllers/BrandController.php
+++ packages/Http/Controllers/BrandController.php
@@ -11,6 +11,7 @@ class BrandController extends Controller
     public function index ($slug)
     {
         $brand = Brand::where('slug',$slug)->first();
+        visitor()->visit($brand);
         return view('http::hall.brand',compact('brand'));
     }
 }
